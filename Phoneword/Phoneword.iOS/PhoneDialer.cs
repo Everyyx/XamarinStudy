184c687ca9c928afd3c04099ef8adeba994ab28a
Everyyx <Every_126@outlook.com>
2018-08-13 16:32:18 +0800

    添加项目文件。

---
+++ Phoneword/Phoneword.iOS/PhoneDialer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using Foundation;
+using UIKit;
+using Phoneword.iOS;
+
+[assembly:Dependency(typeof(PhoneDialer))]
+namespace Phoneword.iOS
+{
+    public class PhoneDialer : IDialer
+    {
+        public bool Dial(string number)
+        {
+            return UIApplication.SharedApplication.OpenUrl(
+                new NSUrl("tel:" + number));
+        }
+    }
+}
---
+++ Phoneword/Phoneword/PhonewordTranslator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Phoneword
+{
+    public static class PhonewordTranslator
+    {
+        public static string ToNumber(string raw)
+        {
+            if (string.IsNullOrWhiteSpace(raw))
+                return null;
+            raw = raw.ToUpperInvariant();
+
+            var newNumer = new StringBuilder();
+            foreach(var c in raw)
+            {
+                if ("-0123456789".Contains(c))
+                    newNumer.Append(c);
+                else
+                {
+                    var result = TranslateToNumer(c);
+                    if (result!=null)
+                        newNumer.Append(result);
+                        //bad character?
+                        else
+                            return null;
+                }
+            }
+            return newNumer.ToString();
+        }
+
+        static bool Contains(this string keyString,char c)
+        {
+            return keyString.IndexOf(c) >= 0;
+        }
+
+        static readonly string[] digits =
+        {
+            "ABC","DEF","GHI","JKL","MNO","PQRS","TUV","WXYZ"
+        };
+
+        static int? TranslateToNumer(char c)
+        {
+            for (int i = 0; i < digits.Length; i++)
+            {
+                if (digits[i].Contains (c))
+                {
+                    return 2 + i;
+                }
+            }
+            return null;
+        }
+    }
+}
